4134b31de48661da358652e6a64a06116bc5f71a
dan <contact@danmyers.net>
2024-10-17 21:56:07 -0500

    updated hypr

--- config/hypr.nix
+++ config/hypr.nix
@@ -189,8 +189,8 @@
     bind = $mainMod ALT, F, togglefloating
     bind = $mainMod, F, fullscreen, 1
     bind = $mainMod CTRL, F, fullscreen
-    bind = $mainMod SHIFT, H, movetoworkspace, 1
-    bind = $mainMod SHIFT, L, movetoworkspace, 2
+    bind = $mainMod CTRL, L, movetoworkspace, 1
+    bind = $mainMod CTLR, H, movetoworkspace, 2
     
     # Move focus with mainMod + vim binds
     bind = $mainMod, H, movefocus, l
