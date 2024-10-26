21b74b7b0dfb769d75c3d6b64c6f9ef6cdac8f85
dan <contact@danmyers.net>
2024-10-25 20:13:39 -0500

    removed gitignore

---
+++ system/nixstation/configuration.nix
@@ -0,0 +1,110 @@
+{ 
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ... 
+}: {
+
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./packages.nix
+      ../config/steam.nix
+    ];
+
+### --- Boot loader --- ###
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+### --- Bluetooth --- ###
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+### --- Localization --- ###
+  time.timeZone = "America/Chicago";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+### --- Xserver setup --- ###
+  services.xserver.enable = true;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+### --- Gnome --- ###
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+### --- Hyprland --- ###
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  # Optional, hint Electron apps to use Wayland:
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+### --- Crucial Programs --- ###
+  programs.firefox.enable = true;
+  environment.variables.EDITOR = "nvim";
+
+### --- Virtualisation --- ###
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+  # Enable USB redirection
+  # virtualisation.spiceUSBRedirection.enable = true;
+
+### --- Networking --- ###
+  networking.hostName = "nixstation";
+  networking.networkmanager.enable = true;
+  networking.firewall = {
+    enable = true;
+    #allowedTCPPorts = [ 80 443 ];
+    #allowedUDPPortRanges = [
+      #{ from = 4000; to = 4007; }
+      #{ from = 8000; to = 8010; }
+    #];
+  };
+
+### --- VPN --- ###
+  services.mullvad-vpn.enable = true;
+  environment.etc.openvpn.source = "${pkgs.update-resolv-conf}/libexec/openvpn";
+
+### --- Audio --- ###
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  
+### --- Package settings --- ###
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = false;
+  
+### --- User setup --- ###
+  users.users.dan = {
+    isNormalUser = true;
+    description = "Dan";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+  };
+
+### --- Version --- ###
+  system.stateVersion = "24.05";
+}
