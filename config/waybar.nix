f4df88c4360c9e8195028600f268e8e5cb46421c
dan <contact@danmyers.net>
2024-10-28 16:39:15 -0500

    updated waybar

--- config/waybar.nix
+++ config/waybar.nix
@@ -45,7 +45,7 @@
       network = {
         interval = 1;
         format-alt = "{ifname}: {ipaddr}/{cidr}";
-        format-disconnected = "Disconnected ⚠";
+        format-disconnected = "Disconnected ⚠  |";
         format-ethernet = "{ifname}: {ipaddr}/{cidr}   up: {bandwidthUpBits} down: {bandwidthDownBits}";
         format-linked = "{ifname} (No IP) ";
         format-wifi = "{essid} ({signalStrength}%)     |";
