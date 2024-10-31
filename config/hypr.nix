a03649022ce28d84f2de0771197afa1986f05e3b
dan <contact@danmyers.net>
2024-10-30 22:21:34 -0500

    updated monitors

--- config/hypr.nix
+++ config/hypr.nix
@@ -22,11 +22,8 @@
     ################
     
     # https://wiki.hyprland.org/Configuring/Monitors/
-    monitor= DP-1, 1920x1080, 0x0, 1
-
+    monitor = DP-1, 1920x1080, 0x0, 1
     monitor = HDMI-A-1, 1920x1080, 1920x0, 1
-    #monitor = HDMI-A-1, disable #1920x1080, 1920x0, 1
-    
     
     ###################
     ### MY PROGRAMS ###
