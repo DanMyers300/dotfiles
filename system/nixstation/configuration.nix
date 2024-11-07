0b1d48a9e317d4fd224091d981ed0ac97a5e4c0e
dan <contact@danmyers.net>
2024-11-06 19:55:49 -0600

    added udev rules

--- system/nixstation/configuration.nix
+++ system/nixstation/configuration.nix
@@ -94,6 +94,13 @@
   services.mullvad-vpn.enable = true;
   environment.etc.openvpn.source = "${pkgs.update-resolv-conf}/libexec/openvpn";
 
+### --- UDEV --- ###
+# For game controllers
+  services.udev.packages = [
+    pkgs.adafruit-udev-rules
+    pkgs.steamcontroller-udev-rules
+  ];
+
 ### --- Audio --- ###
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
