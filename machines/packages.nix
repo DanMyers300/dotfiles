0a5f501778c41b365361b8767fd149c1a29493d6
dan <contact@danmyers.net>
2024-12-03 12:07:44 -0600

    updated packages

--- machines/packages.nix
+++ machines/packages.nix
@@ -4,46 +4,45 @@
 } : {
 
 environment.systemPackages = with pkgs; [
-  qemu
+  # System
   nvtopPackages.amd
-  swtpm
-  git
-  p7zip
-  gcc
-  linuxKernel.packages.linux_zen.xpadneo
-  #rust-bin.stable.latest.default     -- Enable if using flake overlay
-  rustup
-  ## Arduino
-  avrdude
-  pkgsCross.avr.buildPackages.gcc
-  ollama
-  signal-desktop
-  r2modman
-  gnome-extension-manager
-  prismlauncher
-  wl-clipboard
-  nerdfonts
-  sway-contrib.grimshot
-  hyprlock
-  wofi
-  luaformatter
-  webcord-vencord
-  ungoogled-chromium
-  libreoffice
   btop
   baobab
   ripgrep
   bluez-tools
   mullvad-vpn
   pavucontrol
-  python3
-  typescript
-  unstable.deno
-  jdk
-  lua54Packages.lua
-  lua54Packages.luacheck
+  wl-clipboard
+  nerdfonts
+  sway-contrib.grimshot
+  gnome-extension-manager
+  hyprlock
+  wofi
   git
   p7zip
   gcc
+
+  # Game pads
+  linuxKernel.packages.linux_zen.xpadneo
+
+  # Programming lang
+  #rust-bin.stable.latest.default     -- Enable if using flake overlay
+  rustup
+  lua54Packages.lua
+
+  ## Arduino
+  avrdude
+  pkgsCross.avr.buildPackages.gcc
+
+  # Virtualization
+  qemu
+  swtpm
+
+  # MISC
+  ollama
+  signal-desktop
+  r2modman
+  prismlauncher
+  libreoffice
 ];
 }
