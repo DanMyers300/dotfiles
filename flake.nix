8126d8e924b0df4ecfb7b62c9cb002d1e0718ba2
dan <contact@danmyers.net>
2024-12-06 10:50:47 -0600

    stylix fixed

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
-    stylix.url = "github:danth/stylix/release-24.05";
+    stylix.url = "github:danth/stylix/release-24.11";
 
     # For Ardiunio
     ravedude.url = "github:Rahix/avr-hal?dir=ravedude";
