04c0a646ff7e5047ef67d085c9c373df6d6913e7
dan <contact@danmyers.net>
2024-12-08 17:02:26 -0600

    update suspend/sleep settings

--- machines/nixtop.nix
+++ machines/nixtop.nix
@@ -76,6 +76,12 @@
 ### --- Gnome --- ###
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 
 ### --- Hyprland --- ###
   programs.hyprland = {
