b9d7e2692e7245c2dacd0f25b203afdef37f018c
dan <contact@danmyers.net>
2024-12-09 20:13:17 -0600

    updated nixtop

--- machines/nixtop.nix
+++ machines/nixtop.nix
@@ -76,16 +76,28 @@
 ### --- Gnome --- ###
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
-  # If no user is logged in, the machine will power down after 20 minutes.
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
+  programs.dconf.enable = true;
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gedit # text editor
+    cheese # webcam tool
+    gnome-music
+    epiphany # web browser
+    geary # email reader
+    gnome-characters
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+    yelp # Help view
+    gnome-contacts
+    gnome-initial-setup
+  ]);
 
 ### --- Hyprland --- ###
   programs.hyprland = {
-    enable = true;
+    enable = false;
     xwayland.enable = true;
   };
   # Optional, hint Electron apps to use Wayland:
