413302cce44b80eacd9f2e12c941bb2a69eca4cb
dan <contact@danmyers.net>
2024-12-18 15:07:09 -0600

    updated vimrc

--- pkgs/nvim/nvimrc.nix
+++ pkgs/nvim/nvimrc.nix
@@ -18,306 +18,280 @@ programs.neovim.extraConfig = ''
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 
-    set shell=cmd
+set shell=cmd
 
-    set nocompatible
+set nocompatible
 
-    set visualbell
+set visualbell
 
-    autocmd Filetype * setlocal formatoptions-=c formatoptions-=r  formatoptions-=o
+autocmd Filetype * setlocal formatoptions-=c formatoptions-=r  formatoptions-=o
 
-    set encoding=UTF-8
+set encoding=UTF-8
 
-    filetype on
+filetype on
 
-    set nospell
+set nospell
 
-    set smarttab
+set smarttab
 
-    set path+=**
+set path+=**
 
-    filetype plugin on
+filetype plugin on
 
-    filetype indent on
+filetype indent on
 
-    syntax on
+syntax on
 
-    set number relativenumber
+set number relativenumber
 
-    set mouse=a
+set mouse=a
 
-    colorscheme slate 
+colorscheme slate
 
-    "set cursorline
+"set cursorline
 
-    "set cursorcolumn
+"set cursorcolumn
 
-    set shiftwidth=4 expandtab
-    set tabstop=4 expandtab
+set shiftwidth=4 expandtab
+set tabstop=4 expandtab
 
-    autocmd Filetype html setlocal tabstop=2 shiftwidth=2 expandtab
+autocmd Filetype html setlocal tabstop=2 shiftwidth=2 expandtab
 
-    set list lcs=trail:·,tab:»·,lead:·,leadmultispace:·
+set list lcs=trail:·,tab:»·,lead:·,leadmultispace:·
 
-    set nobackup
+set nobackup
 
-    set nowrap 
+set nowrap 
 
 " While searching though a file incrementally highlight matching characters as you type.
-    set incsearch
-    set hlsearch
-    set ignorecase
-    set showmatch
+set incsearch
+set hlsearch
+set ignorecase
+set showmatch
 
 " Show partial command you type in the last line of the screen.
-    set showcmd
+set showcmd
 
 " Show the mode you are on the last line.
-    set showmode
+set showmode
 
 " Set the commands to save in history default number is 20.
-    set history=1000
+set history=1000
 
 " Setting the split window to open as i like (like in a WM - qtile)
-    set splitbelow splitright
+set splitbelow splitright
 
 " Enable auto completion menu after pressing TAB.
-    set wildmenu
+set wildmenu
 
 " There are certain files that we would never want to edit with Vim.
 " Wild menu will ignore files with these extensions.
-    set wildignore=*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx
+set wildignore=*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx
 
 " If Vim version is equal to or greater than 7.3 enable undo file.
 " This allows you to undo changes to a file even after saving it.
-    if version >= 703
-        set undodir=~/.vim/backup
-        set undofile
-        set undoreload=10000
-    endif
+if version >= 703
+    set undodir=~/.vim/backup
+    set undofile
+    set undoreload=10000
+endif
 
 " File Browsing settings
-    let g:netrw_banner=0
-    let g:netrw_liststyle=3
-    let g:netrw_showhide=1
-    let g:netrw_winsize=20
+let g:netrw_banner=0
+let g:netrw_liststyle=3
+let g:netrw_showhide=1
+let g:netrw_winsize=20
 
 " Auto Completion - Enable Omni complete features
-    set omnifunc=syntaxcomplete#Complete
+set omnifunc=syntaxcomplete#Complete
 
-    " Enable Spelling Suggestions for Auto-Completion:
-    set complete+=k
-    set completeopt=menu,menuone,noinsert
+" Enable Spelling Suggestions for Auto-Completion:
+set complete+=k
+set completeopt=menu,menuone,noinsert
 
-    inoremap <expr> <Tab> TabComplete()
-    fun! TabComplete()
-        if getline('.')[col('.') - 2] =~ '\K' || pumvisible()
-            return "\<C-N>"
-        else
-            return "\<Tab>"
-        endif
-    endfun
+inoremap <expr> <Tab> TabComplete()
+fun! TabComplete()
+    if getline('.')[col('.') - 2] =~ '\K' || pumvisible()
+        return "\<C-N>"
+    else
+        return "\<Tab>"
+    endif
+endfun
 
-    inoremap <expr> <CR> pumvisible() ? "\<C-Y>" : "\<CR>"
-    autocmd InsertCharPre * call AutoComplete()
-    fun! AutoComplete()
-        if v:char =~ '\K'
-            \ && getline('.')[col('.') - 4] !~ '\K'
-            \ && getline('.')[col('.') - 3] =~ '\K'
-            \ && getline('.')[col('.') - 2] =~ '\K'
-            \ && getline('.')[col('.') - 1] !~ '\K'
-            call feedkeys("\<C-N>", 'n')
-        end
-    endfun
+inoremap <expr> <CR> pumvisible() ? "\<C-Y>" : "\<CR>"
+autocmd InsertCharPre * call AutoComplete()
+fun! AutoComplete()
+    if v:char =~ '\K'
+        \ && getline('.')[col('.') - 4] !~ '\K'
+        \ && getline('.')[col('.') - 3] =~ '\K'
+        \ && getline('.')[col('.') - 2] =~ '\K'
+        \ && getline('.')[col('.') - 1] !~ '\K'
+        call feedkeys("\<C-N>", 'n')
+    end
+endfun
 
 "------------------STATUS_LINE------------------
 
 
 " Status line
-    set laststatus=2
-    set statusline=
-    set statusline+=%2*
-    set statusline+=%{StatuslineMode()}
-    set statusline+=\ 
-    set statusline+=%{SpellCheckStatus()}
-    set statusline+=%1*
-    set statusline+=\ 
-    set statusline+=%3*
-    set statusline+=<
-    set statusline+=-
-    set statusline+=\ 
-    set statusline+=%f
-    set statusline+=\ 
-    set statusline+=-
-    set statusline+=>
-    set statusline+=\ 
-    set statusline+=%4*
-    set statusline+=%m
-    set statusline+=%=
-    set statusline+=%h
-    set statusline+=%r
-    set statusline+=%4*
-    set statusline+=%c
-    set statusline+=/
-    set statusline+=%l
-    set statusline+=/
-    set statusline+=%L
-    set statusline+=\ 
-    set statusline+=%1*
-    set statusline+=|
-    set statusline+=%y
-    set statusline+=\ 
-    set statusline+=%4*
-    set statusline+=%P
-    set statusline+=\ 
-    set statusline+=%3*
-    set statusline+=t:
-    set statusline+=%n
-    set statusline+=\ 
+set laststatus=2
+set statusline=
+set statusline+=%2*
+set statusline+=%{StatuslineMode()}
+set statusline+=\ 
+set statusline+=%{SpellCheckStatus()}
+set statusline+=%1*
+set statusline+=\ 
+set statusline+=%3*
+set statusline+=<
+set statusline+=-
+set statusline+=\ 
+set statusline+=%f
+set statusline+=\ 
+set statusline+=-
+set statusline+=>
+set statusline+=\ 
+set statusline+=%4*
+set statusline+=%m
+set statusline+=%=
+set statusline+=%h
+set statusline+=%r
+set statusline+=%4*
+set statusline+=%c
+set statusline+=/
+set statusline+=%l
+set statusline+=/
+set statusline+=%L
+set statusline+=\ 
+set statusline+=%1*
+set statusline+=|
+set statusline+=%y
+set statusline+=\ 
+set statusline+=%4*
+set statusline+=%P
+set statusline+=\ 
+set statusline+=%3*
+set statusline+=t:
+set statusline+=%n
+set statusline+=\ 
 
 " Mode
-    function! StatuslineMode()
-      let l:mode=mode()
-      if l:mode==#"n"
-        return "NORMAL"
-      elseif l:mode==#"V"
-        return "VISUAL LINE"
-      elseif l:mode==?"v"
-        return "VISUAL"
-      elseif l:mode==#"i"
-        return "INSERT"
-      elseif l:mode ==# "\<C-V>"
-        return "V-BLOCK"
-      elseif l:mode==#"R"
-        return "REPLACE"
-      elseif l:mode==?"s"
-        return "SELECT"
-      elseif l:mode==#"t"
-        return "TERMINAL"
-      elseif l:mode==#"c"
-        return "COMMAND"
-      elseif l:mode==#"!"
-        return "SHELL"
-      else
-          return "VIM"
-      endif
-    endfunction
+function! StatuslineMode()
+  let l:mode=mode()
+  if l:mode==#"n"
+    return "NORMAL"
+  elseif l:mode==#"V"
+    return "VISUAL LINE"
+  elseif l:mode==?"v"
+    return "VISUAL"
+  elseif l:mode==#"i"
+    return "INSERT"
+  elseif l:mode ==# "\<C-V>"
+    return "V-BLOCK"
+  elseif l:mode==#"R"
+    return "REPLACE"
+  elseif l:mode==?"s"
+    return "SELECT"
+  elseif l:mode==#"t"
+    return "TERMINAL"
+  elseif l:mode==#"c"
+    return "COMMAND"
+  elseif l:mode==#"!"
+    return "SHELL"
+  else
+      return "VIM"
+  endif
+endfunction
 
 " Spell Check Status
-    function! SpellCheckStatus()
-        if &spell 
-            return " [SPELL]"
-        else
-            return ""
-        endif   
-    endfunction
-
-
-"------------------COLORS------------------------
-
-" Vim System
-    hi NonText ctermfg=grey
-    hi Visual ctermbg=white
-    hi Visual ctermfg=black
-    hi Error cterm=reverse ctermfg=white ctermbg=black
-    hi Search ctermfg=lightyellow ctermbg=blue
-    hi IncSearch ctermfg=lightyellow ctermbg=blue
-    hi Pmenu ctermbg=black
-
-" Language Syntax
-    hi Statement ctermfg=magenta
-    hi String ctermfg=green
-    hi Function ctermfg=lightblue
-    hi Operator ctermfg=darkcyan
-    hi Identifier ctermfg=cyan
-    hi MatchParen ctermfg=lightyellow ctermbg=blue
+function! SpellCheckStatus()
+    if &spell 
+        return " [SPELL]"
+    else
+        return ""
+    endif   
+endfunction
 
 "------------------KEY_BINDINGS------------------
 
 
 " Spell-check on\off
-    map <C-z> :setlocal spell! spelllang=en_us<CR>
+map <C-z> :setlocal spell! spelllang=en_us<CR>
+
+" Format a paragraph into lines
+map Q gq<CR>
 
 " Set the space  as the leader key.
-    let mapleader = " "
+let mapleader = " "
 
-" Select all the text
-    nnoremap <leader>a ggVG
-
-" Opening a file explorer
-    map <leader>e :Lex<CR>
-
-" Opening a file from explorer
-    map <leader>o :Explore<CR>
+" Open oil.nvim
+map <leader>o :Oil<CR>
 
 " Split the window. y - y axis | x - x axis 
-    map <leader>y :split<space><CR>
-    map <leader>x :vsplit<space><CR>
+map <leader>y :split<space><CR>
+map <leader>x :vsplit<space><CR>
 
 " Navigate the split view easier by pressing CTRL+j, CTRL+k, CTRL+h, or CTRL+l.
-    nnoremap <c-j> <c-w>j
-    nnoremap <c-k> <c-w>k
-    nnoremap <c-h> <c-w>h
-    nnoremap <c-l> <c-w>l
+nnoremap <c-j> <c-w>j
+nnoremap <c-k> <c-w>k
+nnoremap <c-h> <c-w>h
+nnoremap <c-l> <c-w>l
 
 " Resize split windows using arrow keys by pressing:
 " CTRL+UP, CTRL+DOWN, CTRL+LEFT, or CTRL+RIGHT.
-    noremap <a-up> <c-w>+
-    noremap <a-down> <c-w>-
-    noremap <a-left> <c-w>>
-    noremap <a-right> <c-w><
+noremap <a-up> <c-w>+
+noremap <a-down> <c-w>-
+noremap <a-left> <c-w>>
+noremap <a-right> <c-w><
 
 " Moving between tabs
-    map <leader>t gt
+map <leader>t gt
 
 " Switching buffers
-    map <leader>h :bprev<CR>
-    map <leader>l :bprev<CR>
+map <leader>h :bp<CR>
+map <leader>l :bn<CR>
 
 " Opening\Creating a file in a new tab - write the tab to open
-    noremap <leader>c :tabedit<space>
-
-" Saving a file using CTRL+S
-    map <C-S> :w<CR>
-
-" Quitting and saving a file using CTRL+q
-    map <C-q> :wq<CR>
+noremap <leader>c :tabedit<space>
 
 " Surround word with a wanted character
-    nnoremap <leader>sw <cmd>echo "Press a character: " \| let c = nr2char(getchar()) \| exec "normal viwo\ei" . c . "\eea" . c . "\e" \| redraw<CR>
+nnoremap <leader>sw <cmd>echo "Press a character: " \| let c = nr2char(getchar()) \| exec "normal viwo\ei" . c . "\eea" . c . "\e" \| redraw<CR>
 
 " Replace all occurrences of a word
-    nnoremap <leader>rw :%s/\<<c-r><c-w>\>//g<left><left>
+nnoremap <leader>rw :%s/\<<c-r><c-w>\>//g<left><left>
 
 " Map V-Block to not confuse with paste
-    noremap <leader>v <C-v>
+noremap <leader>v <C-v>
 
 " For copy and paste
-    map <C-V> "+P
-    vnoremap <C-C> "+y :let @+=@*<CR>
+map <C-V> "+P
+vnoremap <C-C> "+y :let @+=@*<CR>
 " If not in Linux replace the keybinding in above line with: vnoremap <C-C> "+y
 
 " Display the registers
-    nnoremap <leader>r <cmd>registers<CR>
+nnoremap <leader>r <cmd>registers<CR>
 
 " Clear search results
-    map <leader>b <cmd>noh<CR>
+map <leader>b <cmd>noh<CR>
 
 " WhichKey
-    nnoremap <silent> <leader> :WhichKey '<Space>'<CR>
+nnoremap <silent> <leader> :WhichKey '<Space>'<CR>
 
 " Lazy
-    nnoremap <silent> <leader>m :Lazy <CR>
+nnoremap <silent> <leader>m :Lazy <CR>
 
 " Telescope
-    nnoremap <leader>ff <cmd>Telescope find_files<cr>
-    nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-    nnoremap <leader>fb <cmd>Telescope buffers<cr>
-    nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+nnoremap <leader>ff <cmd>Telescope find_files<cr>
+nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+nnoremap <leader>fb <cmd>Telescope buffers<cr>
+nnoremap <leader>fh <cmd>Telescope help_tags<cr>
 
 " Output the current syntax group
-    nnoremap <f10> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<'
-    \ . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
-    \ . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">"<cr>
+nnoremap <f10> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<'
+\ . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
+\ . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">"<cr>
+
+" Exit term mode
+tnoremap <Esc> <C-\><C-n>
 '';
 }
