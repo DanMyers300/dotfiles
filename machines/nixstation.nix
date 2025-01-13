ffc92aa384ac6e3b28dc83c5c474fbfadb9ab5f7
dan <contact@danmyers.net>
2025-01-13 16:09:23 -0600

    updated init.lua for nivm

--- machines/nixstation.nix
+++ machines/nixstation.nix
@@ -93,11 +93,11 @@
     networkmanager.enable = true;
     firewall = {
       enable = true;
-      #allowedTCPPorts = [ 22 ];
-      #allowedUDPPortRanges = [
-        #{ from = 4000; to = 4007; }
+      allowedTCPPorts = [ 4321 ];
+      allowedUDPPortRanges = [
+        { from = 4321; to = 4321; }
         #{ from = 8000; to = 8010; }
-      #];
+      ];
     };
     extraHosts =
       ''
