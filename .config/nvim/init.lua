faf7e7e1810ef9e8c73353143bdb17c7f409f8ed
dan <contact@danmyers.net>
2025-01-16 12:05:01 -0600

    updated tabs -> 2 spaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,10 @@
+
 vim.opt.relativenumber = true
+
 vim.opt.wrap = false
+
+-- Set tab width to 2 spaces
+vim.opt.tabstop = 2      -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 2   -- Number of spaces to use for autoindent
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.smarttab = true  -- Use shiftwidth for inserting tabs
