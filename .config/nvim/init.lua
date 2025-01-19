bf046e0e6dafe35fdbb3176b754b952ec66c38ae
dan <contact@danmyers.net>
2025-01-18 18:31:32 -0600

    added Explore keybind for neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,13 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 vim.opt.relativenumber = true
 
 vim.opt.wrap = false
 
--- Set tab width to 2 spaces
 vim.opt.tabstop = 2      -- Number of spaces a tab counts for
 vim.opt.shiftwidth = 2   -- Number of spaces to use for autoindent
 vim.opt.expandtab = true -- Convert tabs to spaces
 vim.opt.smarttab = true  -- Use shiftwidth for inserting tabs
+
+vim.api.nvim_set_keymap('n', '<leader>o', ':Explore<CR>', { noremap = true, silent = true })
