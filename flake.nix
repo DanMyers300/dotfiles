8d5d7fe4181927253bcae962bf4398b400bb9a74
dan <contact@danmyers.net>
2025-01-21 10:39:25 -0600

    created reusable machine function

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
 #             Dan's Nixos Flake
 # -------------------------------------------
 #
+
 {
 
   inputs = {
@@ -27,48 +28,43 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     stylix.url = "github:danth/stylix/release-24.11";
-    #private.url = "git+file:///home/dan/.private";
-    ghostty = {
-      url = "github:ghostty-org/ghostty";
-    };
-
-    # For Ardiunio
+    ghostty.url = "github:ghostty-org/ghostty";
     ravedude.url = "github:Rahix/avr-hal?dir=ravedude";
   };
 
   outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    home-manager,
-    #private,
-    stylix,
-    ghostty,
-    ...
-  } @ inputs: let inherit (self) outputs;
+  self,
+  nixpkgs,
+  nixpkgs-unstable,
+  home-manager,
+  stylix,
+  ghostty,
+  ravedude,
+  ...
+  } @ inputs:
+    let
+      inherit (self) outputs;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      ravedudePkg = ravedude.packages."${system}".default;
 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    ravedude = ravedude.packages."${system}".default;
+      unstableOverlay = {
+        nixpkgs.overlays = [
+          (final: prev: {
+            unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          })
+        ];
+      };
 
-    unstableOverlay = {
-      nixpkgs.overlays = [
-        (final: prev: {
-          unstable = import nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-          };
-        })
-      ];
-    };
+      machines = [ "nixtop" "nixstation" ];
 
-  in {
-
-    nixosConfigurations = {
-      nixtop = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
+      mkNixosConfig = machine: nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs outputs; };
         modules = [
-          ./machines/nixtop.nix
+          (./machines + "/${machine}.nix")
           home-manager.nixosModules.home-manager
           unstableOverlay
           stylix.nixosModules.stylix
@@ -76,28 +72,15 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.dan = import ./home-manager/home.nix;
-            home-manager.extraSpecialArgs = {inherit inputs outputs;};
+            home-manager.extraSpecialArgs = { inherit inputs outputs; };
           }
         ];
       };
-    };
 
-    nixosConfigurations = {
-      nixstation = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./machines/nixstation.nix
-          home-manager.nixosModules.home-manager
-          unstableOverlay
-          stylix.nixosModules.stylix
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.dan = import ./home-manager/home.nix;
-            home-manager.extraSpecialArgs = {inherit inputs outputs;};
-          }
-        ];
-      };
+    in {
+      nixosConfigurations = builtins.listToAttrs (map (machine: {
+        name = machine;
+        value = mkNixosConfig machine;
+      }) machines);
     };
-  };
 }
