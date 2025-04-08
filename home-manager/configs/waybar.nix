d20e0ce90bc025fb16f9f467909afae9cd5db29f
dan <contact@danmyers.net>
2025-04-07 20:40:29 -0500

    fixed spacing issue

--- home-manager/configs/waybar.nix
+++ home-manager/configs/waybar.nix
@@ -32,22 +32,22 @@
           tooltip-format = "{:%Y-%m-%d | %H:%M}";
         };
         cpu = {
-          format = "{usage}%    |";
+          format = "  {usage}%    |";
           tooltip = false;
         };
-        memory = { format = "{}%    |"; };
+        memory = { format = "  {}%    |  "; };
         network = {
           interval = 1;
-          format-alt = "{ifname}: {ipaddr}/{cidr}  |";
-          format-disconnected = "Disconnected ⚠   |";
-          format-ethernet = "{ifname}: {ipaddr}/{cidr}  up: {bandwidthUpBits} down: {bandwidthDownBits}";
-          format-linked = "{ifname} (No IP) ";
-          format-wifi = "{essid} ({signalStrength}%)     |";
+          format-alt = " {ifname}: {ipaddr}/{cidr}  |";
+          format-disconnected = "  Disconnected ⚠   |";
+          format-ethernet = "  {ifname}: {ipaddr}/{cidr}  up: {bandwidthUpBits} down: {bandwidthDownBits}";
+          format-linked = "  {ifname} (No IP) ";
+          format-wifi = "  {essid} ({signalStrength}%)     |";
         };
         pulseaudio = {
-          format = "|  {volume}% {icon}    |  {format_source}  |";
-          format-bluetooth = "|  {volume}% {icon}    |  {format_source}  |";
-          format-bluetooth-muted = " {icon} {format_source}";
+          format = "  |  {volume}% {icon}    |  {format_source}  |";
+          format-bluetooth = "  |  {volume}% {icon}    |  {format_source}  |";
+          format-bluetooth-muted = "   {icon} {format_source}";
           format-icons = {
             car = "";
             default = [ "" "" "" ];
