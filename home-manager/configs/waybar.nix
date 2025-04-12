53b3ea55c6e8a2d42af143e53aeae3c01eff7a12
dan <contact@danmyers.net>
2025-04-12 15:21:54 -0500

    updated spacing for nixtop

--- home-manager/configs/waybar.nix
+++ home-manager/configs/waybar.nix
@@ -76,8 +76,8 @@
             warning = 30;
             critical = 15;
           };
-          format = "{icon}{capacity}%|";
-          format-charging = "⚡{capacity}%|";
+          format = "{icon}  {capacity}%  |  ";
+          format-charging = "⚡{capacity}%  |  ";
           format-plugged = "{capacity}%";
           format-alt = "{time} {icon}";
           format-icons = [ " " " " " " " " " " ];
