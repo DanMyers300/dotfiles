680b5d5ecbd6afade9bc81886080e05b06ba42ad
Dan <contact@danmyers.net>
2026-01-05 18:02:07 -0600

    removed code companion for the time being

--- home-manager/configs/nvim.nix
+++ home-manager/configs/nvim.nix
@@ -105,46 +105,46 @@
           })
           EOF'';
       }
-      {
-        plugin = codecompanion-nvim;
-        config = ''
-        lua << EOF
-        require("codecompanion").setup({
-          adapters = {
-            http = {
-              opts = {show_defaults = false,},
-              ollama_adapter = function()
-                return require("codecompanion.adapters").extend("ollama", {
-                  name = "gemma3:12b",
-                  schema = {
-                    model = {
-                      default = "gemma3:12b",
-                    },
-                    num_ctx = {
-                      default = 16384,
-                    },
-                    num_predict = {
-                      default = -1,
-                    },
-                  },
-                })
-              end,
-            },
-          },
-          strategies = {
-            chat = {
-              adapter = "ollama_adapter",
-            },
-            inline = {
-              adapter = "ollama_adapter",
-            },
-            cmd = {
-              adapter = "ollama_adapter",
-            }
-          },
-        })
-        EOF'';
-      }
+      #{
+      #  plugin = codecompanion-nvim;
+      #  config = ''
+      #  lua << EOF
+      #  require("codecompanion").setup({
+      #    adapters = {
+      #      http = {
+      #        opts = {show_defaults = false,},
+      #        ollama_adapter = function()
+      #          return require("codecompanion.adapters").extend("ollama", {
+      #            name = "gemma3:12b",
+      #            schema = {
+      #              model = {
+      #                default = "gemma3:12b",
+      #              },
+      #              num_ctx = {
+      #                default = 16384,
+      #              },
+      #              num_predict = {
+      #                default = -1,
+      #              },
+      #            },
+      #          })
+      #        end,
+      #      },
+      #    },
+      #    strategies = {
+      #      chat = {
+      #        adapter = "ollama_adapter",
+      #      },
+      #      inline = {
+      #        adapter = "ollama_adapter",
+      #      },
+      #      cmd = {
+      #        adapter = "ollama_adapter",
+      #      }
+      #    },
+      #  })
+      #  EOF'';
+      #}
       {
         plugin = (nvim-treesitter.withPlugins (p: [
           p.tree-sitter-java
