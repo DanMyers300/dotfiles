d047e36805bb071718ff36b1125cf0d5e08b5a39
Dan <contact@danmyers.net>
2026-02-08 14:15:47 -0600

    added auto login for nixstation

--- machines/nixstation.nix
+++ machines/nixstation.nix
@@ -51,6 +51,10 @@
   ### --- Gnome --- ###
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "dan";
+  };
   programs.dconf.enable = true;
   environment.gnome.excludePackages = (
     with pkgs;
