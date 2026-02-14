1f23560b66782da5b398f6da597b89ec2d991463
Dan <contact@danmyers.net>
2026-02-14 11:06:51 -0600

    added uxplay to nixbook

--- machines/nixbook.nix
+++ machines/nixbook.nix
@@ -40,7 +40,23 @@
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 22 ];
+    allowedUDPPorts = [ ];
   };
+
+  ### --- Avahi (for UxPlay AirPlay server) --- ###
+  ### --- Ports: TCP(7000 7001 7100) UDP: 5353 6000 6001 7011
+  #services.avahi = {
+  #  enable = true;
+  #  nssmdns4 = true;
+  #  publish = {
+  #    enable = true;
+  #    addresses = true;
+  #    domain = true;
+  #    hinfo = true;
+  #    userServices = true;
+  #    workstation = true;
+  #  };
+  #};
 
   services.tailscale = {
     enable = true;
