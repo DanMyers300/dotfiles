806d385bc59cfaa7fa596daba7b6d41303361dd4
Dan <contact@danmyers.net>
2026-02-21 10:51:37 -0600

    removed video.mp4

--- home-manager/configs/hypr.nix
+++ home-manager/configs/hypr.nix
@@ -47,7 +47,7 @@ in
     #################
     ### AUTOSTART ###
     #################
-    ${lib.optionalString isNixstation "exec-once = mpvpaper -f -o \"no-audio loop\" '*' ~/.dotfiles/matrix.mp4"}
+    ${lib.optionalString isNixstation "exec-once = mpvpaper -f -o \"no-audio loop\" '*' ~/Videos/matrix.mp4"}
     ${lib.optionalString (!isNixstation) "exec-once = hyprpaper"}
     exec-once = blueman-applet
     exec-once = "hyprctl setcursor rose-pine-hyprcursor 18"
