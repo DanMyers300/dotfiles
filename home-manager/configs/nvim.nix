6b5994dd5b14754ad9e0c5b98b6cebae3b6abc54
Dan <contact@danmyers.net>
2026-03-04 22:58:10 -0600

    experimented with llm auto complete

--- home-manager/configs/nvim.nix
+++ home-manager/configs/nvim.nix
@@ -173,6 +173,41 @@
         );
         config = "";
       }
+      #{
+      #  plugin = unstable.vimPlugins.minuet-ai-nvim;
+      #  config = ''
+      #    lua << EOF
+      #    require('minuet').setup({
+      #      provider = 'openai_compatible',
+      #      n_completions = 1,
+      #      context_window = 512,
+      #      request_timeout = 6,
+      #      notify = 'warn',
+      #      provider_options = {
+      #        openai_compatible = {
+      #          model = 'deepseek-coder:latest',
+      #          end_point = 'http://localhost:11434/v1/chat/completions',
+      #          api_key = 'TERM',
+      #          name = 'Ollama',
+      #          optional = {
+      #            max_tokens = 128,
+      #            top_p = 0.9,
+      #          },
+      #        },
+      #      },
+      #      virtualtext = {
+      #        auto_trigger_ft = { '*' },
+      #        keymap = {
+      #          accept = '<A-a>',
+      #          accept_line = '<A-l>',
+      #          prev = '<A-[>',
+      #          next = '<A-]>',
+      #          dismiss = '<A-e>',
+      #        },
+      #      },
+      #    })
+      #    EOF'';
+      #}
       cmp-nvim-lsp
       cmp-buffer
       cmp-path
