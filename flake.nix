6d9ecf0f032d3b659ec33d0712afbf9310746909
Dan <contact@danmyers.net>
2026-03-30 17:54:20 -0500

    Removed another reference to minegrub

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     zen-browser.url = "github:DanMyers300/zen-browser-flake";
     stylix.url = "github:danth/stylix/release-25.11";
-    minegrub-theme.url = "github:Lxtharia/minegrub-theme";
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
