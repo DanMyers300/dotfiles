d53de55ec56f3aae39af346aa377d52a833416e4
Dan <contact@danmyers.net>
2026-05-02 16:32:27 -0500

    added gamescope

--- pkgs/steam.nix
+++ pkgs/steam.nix
@@ -2,10 +2,12 @@
 
 {
   hardware.steam-hardware.enable = true;
+  programs.gamescope.enable = true;
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+    gamescopeSession.enable = true;
   };
 }
