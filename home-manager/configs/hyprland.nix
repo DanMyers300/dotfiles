910f7ba5ecb4662cd45e37693990301c335b7c4b
Dan <contact@danmyers.net>
2026-05-19 21:02:58 -0500

    switched to scaled setup

--- home-manager/configs/hyprland.nix
+++ home-manager/configs/hyprland.nix
@@ -25,15 +25,15 @@
     ### --- ###
 
     ### - Default Setup ###
-    #monitor = DP-2, 3840x2160@160, 0x0, 2
-    #monitor = HDMI-A-1, 1920x1080@60, 1920x0, 1
-    #monitor = HDMI-A-2, 3840x2160@30, 3840x0, 1
+    monitor = DP-2, 3840x2160@160, 0x0, 2
+    monitor = HDMI-A-1, 1920x1080@60, 1920x0, 1
+    monitor = HDMI-A-2, 3840x2160@30, 3840x0, 1
     ### --- ###
 
     ### - HDMI Mirror Setup ###
-    monitor = DP-2, 3840x2160@160, 0x0, 1
-    monitor = HDMI-A-1, 1920x1080@60, 3840x0, 1
-    monitor = HDMI-A-2, 3840x2160@60, auto, 1, mirror, HDMI-A-1
+    #monitor = DP-2, 3840x2160@160, 0x0, 1
+    #monitor = HDMI-A-1, 1920x1080@60, 3840x0, 1
+    #monitor = HDMI-A-2, 3840x2160@60, auto, 1, mirror, HDMI-A-1
     ### --- ###
 
     debug:disable_scale_checks = true
