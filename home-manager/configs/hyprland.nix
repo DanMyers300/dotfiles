674b1f3fae40ac6d0bf8fff118f35bd1356885df
Dan <contact@danmyers.net>
2026-07-13 13:59:18 -0500

    update

--- home-manager/configs/hyprland.nix
+++ home-manager/configs/hyprland.nix
@@ -24,10 +24,10 @@
     workspace = 3, monitor:HDMI-A-2, default:true
 
     ### - DP-2 Scale 1 Setup ###
-    #monitor = DP-2, 3840x2160@160, 0x0, 1
-    #monitor = HDMI-A-1, 1920x1080@60, 3840x0, 1
-    #monitor = HDMI-A-2, 1920x1080@60, 5760x0, 1
-    #monitor = HDMI-A-3, disable
+    monitor = DP-2, 3840x2160@160, 0x0, 1
+    monitor = HDMI-A-1, 1920x1080@60, 3840x0, 1
+    monitor = HDMI-A-2, 1920x1080@60, 5760x0, 1
+    monitor = HDMI-A-3, disable
     ### --- ###
 
     ### - Default Setup ###
@@ -38,10 +38,10 @@
     ### --- ###
 
     ### - HDMI Mirror Setup ###
-    monitor = DP-2, 3840x2160@160, 0x0, 1
-    monitor = HDMI-A-1, 1920x1080@60, 3840x0, 1
-    monitor = HDMI-A-2, 3840x2160@60, auto, 1, mirror, DP-2
-    monitor = HDMI-A-3, disable
+    #monitor = DP-2, 3840x2160@160, 0x0, 1
+    #monitor = HDMI-A-1, 1920x1080@60, 3840x0, 1
+    #monitor = HDMI-A-2, 3840x2160@60, auto, 1, mirror, HDMI-A-1
+    #monitor = HDMI-A-3, disable
     ### --- ###
 
     ###################
