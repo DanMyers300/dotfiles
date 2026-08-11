eb3ba8baa8e3606145b321d74d0fbf7353cce085
Dan <contact@danmyers.net>
2026-08-11 16:01:56 -0500

    removed kernel from nixbook

--- machines/nixbook.nix
+++ machines/nixbook.nix
@@ -22,8 +22,6 @@
     ../pkgs/regreet.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_6_12;
-
   ### --- Wifi drivers --- ###
   boot.initrd.kernelModules = [ "wl" ];
   boot.kernelModules = [
