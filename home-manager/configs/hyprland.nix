cda0bc070d9e348153c9018a8f67a4790f4c1f7c
Dan <contact@danmyers.net>
2026-08-21 13:11:19 -0500

    Added steam big picture keyboard shortcut

--- home-manager/configs/hyprland.nix
+++ home-manager/configs/hyprland.nix
@@ -218,6 +218,7 @@
     bind = $mainMod, e, exec, $fileManager
     bind = , print, exec, grimshot copy area
     bind = $mainMod CTRL, S, exec, grimshot copy area
+    bind = $mainMod ALT, S, exec, steam -bigpicture
 
     ### --- System --- ###
     bind = $mainMod, Q, killactive,
